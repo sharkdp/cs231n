a07216ddbf1085c1cf64a6f9442ce69565e71938
sharkdp <davidpeter@web.de>
2017-01-06 11:53:31 +0100

    Implement spatial batch normalization

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -604,7 +604,12 @@ def spatial_batchnorm_forward(x, gamma, beta, bn_param):
     # version of batch normalization defined above. Your implementation should  #
     # be very short; ours is less than five lines.                              #
     ##########################################################################
-    pass
+
+    N, C, H, W = x.shape
+    x_tp = x.transpose((0, 2, 3, 1)).reshape((N * H * W, C))
+    out_tp, cache = batchnorm_forward(x_tp, gamma, beta, bn_param)
+    out = out_tp.reshape((N, H, W, C)).transpose((0, 3, 1, 2))
+
     ##########################################################################
     #                             END OF YOUR CODE                              #
     ##########################################################################
@@ -634,7 +639,12 @@ def spatial_batchnorm_backward(dout, cache):
     # version of batch normalization defined above. Your implementation should  #
     # be very short; ours is less than five lines.                              #
     ##########################################################################
-    pass
+
+    N, C, H, W = dout.shape
+    dout_tp = dout.transpose((0, 2, 3, 1)).reshape((N * H * W, C))
+    dx_tp, dgamma, dbeta = batchnorm_backward(dout_tp, cache)
+    dx = dx_tp.reshape((N, H, W, C)).transpose((0, 3, 1, 2))
+
     ##########################################################################
     #                             END OF YOUR CODE                              #
     ##########################################################################
